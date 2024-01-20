4fb011b739cc213ae932d934b0b92bb5a111d416
Jonathan O'Hara <jonathanoh59@gmail.com>
2024-01-20 10:09:51 +0000

    feature list to app.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -78,3 +78,15 @@ function App() {
 }
 
 export default App;
+
+
+//FEATURES TO ADD:
+
+// -Pressing enter triggers a search
+// -Include preview samples for each track
+// -Only display songs not currently present in the playlist in the search results
+// -Add a loading screen while playlist is saving
+// -Update the access token logic to expire at exactly the right time, instead of setting expiration from when the user initiates their next search
+// -After user redirect on login, restoring the search term from before the redirect
+// -Ensure playlist information doesn’t get cleared if a user has to refresh their access token
+// -Provide a way to fetch and see all your existing playlists
